575dff37f615b40cac2ad9810177b44f7fb87a63
kanggaro <tugassmstr2@gmail.com>
2023-05-27 13:05:56 +0700

    minor change

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,7 +9,7 @@
             <h2> Show User</h2>
         </div>
         <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('users.index') }}"> Back</a>
+            <a class="btn btn-primary mb-3" href="{{ route('users.index') }}"> Back</a>
         </div>
     </div>
 </div>
